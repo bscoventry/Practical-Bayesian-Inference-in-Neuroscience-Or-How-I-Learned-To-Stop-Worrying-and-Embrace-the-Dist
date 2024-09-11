f910b3f7dcbfff15d64ca2cbd015fd2606a52c7b
Brandon Coventry <bscoventry@gmail.com>
2024-09-11 17:28:31 -0500

    Added Spline Regression Models!

--- Spline_Regression.py
+++ Spline_Regression.py
@@ -86,16 +86,70 @@ if __name__ == "__main__":
 
     #Setup regression model
     COORDS = {"splines": np.arange(B.shape[1])}
+    pdb.set_trace()
     with pm.Model(coords=COORDS) as spline_model:
         a = pm.Normal("a", 0, 1)
         w = pm.Normal("w", mu=0, sigma=3, size=B.shape[1], dims="splines")
         mu = pm.Deterministic("mu", a + pm.math.dot(np.asarray(B, order="F"), w.T))
         sigma = pm.Exponential("sigma", 1)
-        D = pm.Normal("D", mu=mu, sigma=sigma, observed=df.betaCoh, dims="splines")
+        D = pm.Normal("D", mu=mu, sigma=sigma, observed=df.betaCoh)
 
     with spline_model:
         idata = pm.sample_prior_predictive()
-        idata.extend(pm.sample(draws=1000, tune=1000, random_seed=RANDOM_SEED, chains=4))
+        idata.extend(pm.sample(draws=1000, tune=1000, random_seed=RANDOM_SEED, chains=2,target_accept=0.95,nuts_sampler='numpyro'))
         pm.sample_posterior_predictive(idata, extend_inferencedata=True)
     
+    pdb.set_trace()
+    wp = idata.posterior["w"].mean(("chain", "draw")).values
+
+    spline_df = (
+        pd.DataFrame(B * wp.T)
+        .assign(lepp=df.lepp.values)
+        .melt("lepp", var_name="spline_i", value_name="value")
+    )
+
+    spline_df_merged = (
+        pd.DataFrame(np.dot(B, wp.T))
+        .assign(lepp=df.lepp.values)
+        .melt("lepp", var_name="spline_i", value_name="value")
+    )
+
+
+    color = plt.cm.rainbow(np.linspace(0, 1, len(spline_df.spline_i.unique())))
+    fig = plt.figure()
+    for i, c in enumerate(color):
+        subset = spline_df.query(f"spline_i == {i}")
+        subset.plot("lepp", "value", c=c, ax=plt.gca(), label=i)
+    spline_df_merged.plot("lepp", "value", c="black", lw=2, ax=plt.gca())
+    plt.legend(title="Spline Index", loc="lower center", fontsize=8, ncol=6)
+
+    for knot in knot_list:
+        plt.gca().axvline(knot, color="grey", alpha=0.4)
+    
+    post_pred = az.summary(idata, var_names=["mu"]).reset_index(drop=True)
+    Band_data_post = df.copy().reset_index(drop=True)
+    Band_data_post["pred_mean"] = post_pred["mode"]
+    Band_data_post["pred_hdi_lower"] = post_pred["hdi_5%"]
+    Band_data_post["pred_hdi_upper"] = post_pred["hdi_95%"]
+
+    df.plot.scatter(
+    "lepp",
+    "betaCoh",
+    color="cornflowerblue",
+    s=10,
+    title="BetaCoherence data with posterior predictions",
+    ylabel="Coherence",
+    )
+    for knot in knot_list:
+        plt.gca().axvline(knot, color="grey", alpha=0.4)
+
+    Band_data_post.plot("year", "pred_mean", ax=plt.gca(), lw=3, color="firebrick")
+    plt.fill_between(
+        Band_data_post.lepp,
+        Band_data_post.pred_hdi_lower,
+        Band_data_post.pred_hdi_upper,
+        color="firebrick",
+        alpha=0.4,
+    )
+    plt.show()
     pdb.set_trace()
